8f771fd8a1afd918973e9c6c05165d19466d05fa
Adrian Cockcroft <adrian.cockcroft@gmail.com>
2025-08-18 16:52:35 -0700

    Project structure created

---
+++ bertha/bertha/berthaApp.swift
@@ -0,0 +1,17 @@
+//
+//  berthaApp.swift
+//  bertha
+//
+//  Created by Adrian Cockcroft on 8/18/25.
+//
+
+import SwiftUI
+
+@main
+struct berthaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ bertha/berthaTests/berthaTests.swift
@@ -0,0 +1,17 @@
+//
+//  berthaTests.swift
+//  berthaTests
+//
+//  Created by Adrian Cockcroft on 8/18/25.
+//
+
+import Testing
+@testable import bertha
+
+struct berthaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
